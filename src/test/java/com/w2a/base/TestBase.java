6863355138a8411452438083166b86d8104a0a92
sharathchandraGupta <sharath818@gmail.com>
2019-10-27 14:43:51 +0530

    Space

--- src/test/java/com/w2a/base/TestBase.java
+++ src/test/java/com/w2a/base/TestBase.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
